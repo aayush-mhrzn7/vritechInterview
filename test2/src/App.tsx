984236cd3110137a253081e582733b7b7c8b5784
aayush maharjan <aayushmaharjan75@gmail.com>
2024-12-08 21:10:32 +0545

    fin

--- test2/src/App.tsx
+++ test2/src/App.tsx
@@ -1,8 +1,17 @@
-
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="h-screen font-primary p-10">
+      <input
+        autoFocus
+        type="text"
+        placeholder="Enter a task."
+        className="p-2 border rounded-md rounded-tr-none outline-none rounded-br-none"
+      />
+      <button className="p-2 px-10 rounded-md bg-blue-500 rounded-tl-none rounded-bl-none text-white font-semibold">
+        Add
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
